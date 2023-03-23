cccbcf3be15c8fae2a73cbe43fc771ccf01f7f39
Andre Cox <arc.xoc@gmail.com>
2023-03-23 05:21:49 -0700

    Damage Cooldown

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,6 +7,7 @@ public class PlayerHealth : MonoBehaviour
 {
     public int health = 3;
     public GameObject[] hearts;
+    public float damageCooldownTime = 0.5f;
 
     private Rigidbody2D rb;
 
@@ -15,17 +16,41 @@ public class PlayerHealth : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+    bool invincible = false;
+
     public void takeDamage()
     {
-        health--;
-        hearts[health].SetActive(false);
-        if (health <= 0)
-            die();
-        ScreenShake.instance.shake(0.1f, 0.2f);
-        rb.velocity = Vector3.zero;
+        if (!invincible)
+        {
+            health--;
+            hearts[health].SetActive(false);
+            if (health <= 0)
+                die();
+            ScreenShake.instance.shake(0.1f, 0.2f);
+            rb.velocity = Vector3.zero;
+            invincible = true;
+        }
     }
-    public void die() {
+
+    public void die()
+    {
         GameManager.instance.GameOver();
         Destroy(gameObject);
+    }
+
+    // update is called once per frame
+    float timeSinceLastDamage = 0.0f;
+
+    private void Update()
+    {
+        if (invincible)
+        {
+            timeSinceLastDamage += Time.deltaTime;
+            if (timeSinceLastDamage >= damageCooldownTime)
+            {
+                invincible = false;
+                timeSinceLastDamage = 0.0f;
+            }
+        }
     }
 }
