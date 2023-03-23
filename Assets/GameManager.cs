a1452336681c571e90179350ae7f957d61966d27
pjilekro <pat.ale.jr@gmail.com>
2023-03-22 23:39:02 -0700

    Update

---
+++ Assets/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public bool roundIsActive;
+    public int round;
+
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI roundText;
+
+    private float time;
+    public float roundTime = 45;
+
+    public void BeginRound() {
+        roundIsActive = true;
+        time = roundTime;
+
+        roundText.text = "Round " + round;
+    }
+
+    public void Start()
+    {
+        BeginRound();
+    }
+    public void Update()
+    {
+        if (roundIsActive)
+        {
+            time -= Time.deltaTime;
+            timerText.text = time.ToString("#.00");
+        }
+    }
+}
